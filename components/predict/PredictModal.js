9dd68d85b429ec1c2f7c26b29da76a8eb2c6ae89
Siva794 <ksivanagendraprasad@gmail.com>
2026-03-21 14:44:00 +0530

    Improved Predcit Feature

--- components/predict/PredictModal.js
+++ components/predict/PredictModal.js
@@ -96,11 +96,10 @@ export default function PredictModal({ onClose, data, onApply }) {
     : 0;
 
   return (
-    <div className="fixed inset-0 bg-black/40 flex items-end justify-center z-50 px-3 pt-24 pb-24">
+    <div className="fixed inset-0 bg-black/40 flex items-end justify-center z-50 px-3 pb-24">
 
       {/* MODAL */}
-      <div className="bg-white w-full max-w-sm rounded-xl max-h-[80vh] flex flex-col">
-
+      <div className="bg-white w-full max-w-sm rounded-xl max-h-[calc(100vh-6rem)] flex flex-col">
         {/* HEADER */}
         <div className="px-3 py-2 border-b flex justify-between items-center text-sm">
           <button
